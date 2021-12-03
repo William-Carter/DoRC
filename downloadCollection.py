99dac4bfd4da0458959394f50707d5978ab7c0c7
William Carter <williamcarter808@gmail.com>
2021-12-03 11:23:19 +1000

    Corrected error processing for invalid login

--- downloadCollection.py
+++ downloadCollection.py
@@ -9,22 +9,24 @@ import os
 import json
 dirPath = os.path.dirname(os.path.realpath(__file__))
 
+
 def main():
     argParser = argparse.ArgumentParser()
-    argParser.add_argument("-x", required=True, help="Your credentials file. See the readme for help on formatting it correctly.")
-    argParser.add_argument("-s", required=True, help="The subreddit your collection belongs to.")
-    argParser.add_argument("-c", required=True, help="The ID of your collection. This is the end part of the URL (Without the slashes).")
-    argParser.add_argument("-d", help="The directory to download files to. Will be downloaded in the same folder as the script by default.")
+    argParser.add_argument(
+        "-x", required=True, help="Your credentials file. See the readme for help on formatting it correctly.")
+    argParser.add_argument("-s", required=True,
+                           help="The subreddit your collection belongs to.")
+    argParser.add_argument(
+        "-c", required=True, help="The ID of your collection. This is the end part of the URL (Without the slashes).")
+    argParser.add_argument(
+        "-d", help="The directory to download files to. Will be downloaded in the same folder as the script by default.")
     args = argParser.parse_args()
 
     if not os.path.isfile(args.x):
         raise Exception("Credential file does not exist!")
 
-    
-
     if not os.path.isdir(args.d):
         raise Exception("Invalid Directory")
-
 
     with open(args.x, "r") as credentialsFile:
         credentials = json.load(credentialsFile)
@@ -32,16 +34,20 @@ def main():
     if not (("password" in credentials) and ("username" in credentials) and ("client_id" in credentials) and ("client_secret" in credentials)):
         raise Exception("Credential file is incomplete!")
 
-    downloadCollection(credentials["client_id"], credentials["client_secret"], credentials["username"], credentials["password"], args.s, args.c, args.d)
-    
+    elif not (credentials["password"] and credentials["username"] and credentials["client_id"] and credentials["client_secret"]):
+        raise Exception("Credential file is incomplete!")
+
+    downloadCollection(credentials["client_id"], credentials["client_secret"],
+                       credentials["username"], credentials["password"], args.s, args.c, args.d)
+
 
 def downloadCollection(clientID, clientSecret, username, password, subreddit, collectionID, dlDirectory=""):
     reddit = praw.Reddit(
-    client_id=clientID,
-    client_secret=clientSecret,
-    password=password,
-    user_agent="Collection Downloader",
-    username=username
+        client_id=clientID,
+        client_secret=clientSecret,
+        password=password,
+        user_agent="Collection Downloader",
+        username=username
     )
 
     try:
@@ -55,29 +61,28 @@ def downloadCollection(clientID, clientSecret, username, password, subreddit, co
     except:
         raise Exception("Collection and/or subreddit is incorrect")
 
-
     if not dlDirectory:
         directory = dirPath+"/"+subreddit+"_"+collection.title+"/"
     else:
         directory = dlDirectory+"/"+subreddit+"_"+collection.title+"/"
-    
+
     if not os.path.isdir(directory):
         os.makedirs(directory)
 
     for link in collection.sorted_links:
-        print(list(collection.sorted_links).index(link)+1, "of", len(collection.sorted_links))
+        print(list(collection.sorted_links).index(
+            link)+1, "of", len(collection.sorted_links))
         timeAtStartOfRequest = time.time()
         print("Requesting", link)
         submission = reddit.submission(id=link)
         response = requests.get(submission.url)
-
 
         filename = directory+str(link)+".jpg"
         print("Writing to", filename)
         with open(filename, "wb") as f:
             f.write(response.content)
         print("Downloaded", os.path.getsize(filename), "bytes of data")
-        
+
         # Prevent the 60 request per minute limit from being exceededs
         limiter = max(1-(time.time()-timeAtStartOfRequest), 0)
         print("Waiting for", round(limiter, 2), "seconds\n\n")
